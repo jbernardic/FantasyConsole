31a4055c55e7d39b2b8317282f5794edc07e5e4a
janxy-dev <47927018+jbernardic@users.noreply.github.com>
2022-12-29 19:52:33 +0100

    text editor rewrite, better performance

--- FantasyConsole/TextEditor.cpp
+++ FantasyConsole/TextEditor.cpp
@@ -1,58 +1,56 @@
 #include "TextEditor.h"
 #include "Input.h"
+#include <numeric>
 
 TextEditor::TextEditor(TextRenderer& textRenderer) : m_TextRenderer(textRenderer)
 {
 	m_Cursor.SetColor(1.0, 1.0, 1.0, 1.0);
 	//on key press
 	KeyInputCallback keyCallback = [&](GLFWwindow* window, int key, int scan, int action, int mode) {
-		if (key == GLFW_KEY_BACKSPACE && !Text.empty() && action > 0) { //if BACKSPACE pressed
-			if (Text.back() == '\n') {
+		if (key == GLFW_KEY_BACKSPACE && !Text.empty() && action > 0 && !(CursorPosition.x == 0 && CursorPosition.y == 0)) { //if BACKSPACE pressed
+			if (CursorPosition.x == 0 && CursorPosition.y > 0) {
+				std::string s = Text[CursorPosition.y];
+				Text.erase(Text.begin() + CursorPosition.y);
 				CursorPosition = FindCursor(UINT16_MAX, CursorPosition.y-1);
+				Text[CursorPosition.y] += s;
 			}
-			else CursorPosition.x--;
-			Text.pop_back();
+			else {
+				CursorPosition.x--;
+				Text[CursorPosition.y].erase(CursorPosition.x, 1);
+			}
 		}
 		else if (key == GLFW_KEY_ENTER && action > 0) { //if ENTER pressed
-			Text.push_back('\n');
+			std::string s = Text[CursorPosition.y].substr(CursorPosition.x);
+			Text.insert(Text.begin() + CursorPosition.y+1, s);
+			Text[CursorPosition.y] = Text[CursorPosition.y].substr(0, CursorPosition.x);
 			CursorPosition.x = 0;
 			CursorPosition.y++;
 		}
 		else if (key == GLFW_KEY_LEFT && action > 0) {
-			unsigned int pos = FindCursorPosition(CursorPosition.x, CursorPosition.y);
-			if (pos <= 0) return;
-			if (Text[pos-1] == '\n') {
+			if (CursorPosition.x <= 0 && CursorPosition.y > 0) {
 				CursorPosition = FindCursor(UINT16_MAX, CursorPosition.y-1);
 			}
-			else if(CursorPosition.x>0) CursorPosition.x--;
+			else CursorPosition = FindCursor(CursorPosition.x - 1, CursorPosition.y);
 		}
 		else if (key == GLFW_KEY_RIGHT && action > 0) {
-			unsigned int pos = FindCursorPosition(CursorPosition.x, CursorPosition.y);
-			if (pos < Text.size() && Text[pos] == '\n') {
-				CursorPosition.y++;
-				CursorPosition.x = 0;
+			if (CursorPosition.x >= Text[CursorPosition.y].size() && CursorPosition.y < Text.size() - 1) {
+				CursorPosition = FindCursor(0, CursorPosition.y + 1);
 			}
-			else if (pos < Text.size()) {
-				CursorPosition.x++;
-			}
+			else CursorPosition = FindCursor(CursorPosition.x+1, CursorPosition.y);
 		}
 		else if (key == GLFW_KEY_UP && action > 0 && CursorPosition.y > 0) {
 			CursorPosition = FindCursor(CursorPosition.x, CursorPosition.y-1);
 		}
 		else if (key == GLFW_KEY_DOWN && action > 0) {
-			glm::vec2 cpos = FindCursor(CursorPosition.x, CursorPosition.y+1);
-			if (cpos.y != CursorPosition.y) CursorPosition = cpos;
+			CursorPosition = FindCursor(CursorPosition.x, CursorPosition.y+1);
 		}
 	};
 	//on character type
 	CharacterInputCallback characterCallback = [&](GLFWwindow* window, unsigned int codepoint) {
 		if (codepoint-32 < 224) { //if in character atlas
-			Text.push_back(codepoint);
-			if (codepoint == '\n') {
-				CursorPosition.x = 0;
-				CursorPosition.y++;
-			}
-			else CursorPosition.x++;
+			if (Text.size() <= CursorPosition.y) Text.push_back("");
+			Text[CursorPosition.y].insert(CursorPosition.x, 1, codepoint);
+			CursorPosition.x++;
 		}
 	};
 	Input::AddEventListener("TextEditorKey", keyCallback);
@@ -63,36 +61,23 @@ void TextEditor::Draw()
 {
 	m_Cursor.SetPosition(static_cast<float>(CursorPosition.x*m_TextRenderer.LetterSpacing), static_cast<float>(CursorPosition.y*m_TextRenderer.LineSpacing));
 	m_Cursor.Draw();
-	m_TextRenderer.Draw(Position, Text);
+	if (Text.empty()) return;
+	std::string separator = "\n";
+	std::string result = std::accumulate(Text.begin() + 1, Text.end(), Text[0],
+		[&](const std::string& a, const std::string& b) {
+		return a + separator + b;
+	});
+	m_TextRenderer.Draw(Position, result);
 }
 
-unsigned int TextEditor::FindCursorPosition(unsigned int x, unsigned int y)
+glm::vec2 TextEditor::FindCursor(int x, int y)
 {
-	for (int i = 0; i < Text.size(); i++) {
-		if (x == 0 && y == 0) {
-			return i;
-		}
-		if (Text[i] == '\n') {
-			if (y == 0) return i;
-			y--;
-			continue;
-		}
-		if(y == 0) x--;
-	}
-	return Text.size();
-}
+	if (y < 0) y = 0;
+	if (x < 0) x = 0;
 
-glm::vec2 TextEditor::FindCursor(unsigned int cursorX, unsigned int cursorY)
-{
-	unsigned int x = 0;
-	unsigned int y = 0;
-	unsigned int pos = FindCursorPosition(cursorX, cursorY);
-	for (int i = 0; i < pos; i++) {
-		if (Text[i] == '\n') {
-			y++;
-			x = 0;
-		}
-		else x++;
+	if (y >= Text.size()) {
+		y = Text.size() - 1;
 	}
+	if (x >= Text[y].size()) x = Text[y].size();
 	return glm::vec2(x, y);
 }
--- FantasyConsole/TextEditor.h
+++ FantasyConsole/TextEditor.h
@@ -6,14 +6,12 @@ class TextEditor
 public:
 	TextEditor(TextRenderer& textRenderer);
 	void Draw();
-	std::string Text;
+	std::vector<std::string> Text{ "" };
 	glm::vec2 Position = glm::vec2();
 	//position in Text string
-	glm::vec2 CursorPosition = glm::vec2();
-	//from grid position
-	unsigned int FindCursorPosition(unsigned int x, unsigned int y);
-	//returns closest character to cursor
-	glm::vec2 FindCursor(unsigned int x, unsigned int y);
+	glm::i16vec2 CursorPosition = glm::vec2();
+	//returns closest character to x and y in grid
+	glm::vec2 FindCursor(int x, int y);
 private:
 	TextRenderer& m_TextRenderer;
 	RectangleShape m_Cursor = RectangleShape(static_cast<float>(m_TextRenderer.LetterSpacing), static_cast<float>(m_TextRenderer.FontSize), 0, 0);
